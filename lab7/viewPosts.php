728c8b9bf918136d3ef11185b22340f1ffc4f1f1
tiggesryan <tigges@iastate.edu>
2015-10-25 18:50:43 -0500

    Updates

--- lab7/viewPosts.php
+++ lab7/viewPosts.php
@@ -3,8 +3,13 @@
  * Date: 10/24/2015
  * Time: 1:13 PM
  */
+session_start();
 echo "<h4>Make or view posts</h4>";
-
-
+ $data=file_get_contents('posts.txt');
+ $convertedString=json_encode($data, true);
+//$convertedObj=json_decode($convertedString,true);
+foreach($separate as $post) {
+    echo '<br>' + $post;
+}
 echo "<a href=\"logout.php\">Logout</a>";
 ?>
