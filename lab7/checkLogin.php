3e784bc721c3863aacc2f4aa0951da7904ef919e
Mason Berhenke <stringsanbu@gmail.com>
2015-10-25 09:39:38 -0500

    Fixed login issues

--- lab7/checkLogin.php
+++ lab7/checkLogin.php
@@ -6,17 +6,19 @@
 session_start();
 $user = $_POST['username'];
 $pass = $_POST['password'];
-$userlist = file ('users.txt');
 
+$userlist = file ('users.txt');
 
 $success = false;
 foreach ($userlist as $individual) {
+    $individual = preg_replace("/[\n\r]/", "", $individual);
     $separate = explode(' ', $individual);
     if ($separate[0] == $user && $separate[1] == $pass) {
         $success = true;
         break;
     }
 }
+
 if($success) {
     echo "success";
 }
